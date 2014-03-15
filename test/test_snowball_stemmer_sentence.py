06ee72c9faec0533ab8dd01145a191e63abad083
David Gerő <davidpgero@gmail.com>
2014-03-16 00:45:55 +0100

    Refactor code librarires
    Conflicts:
    	hungarian-nltk/src/snowball_stemmer_sentence.py

---
+++ test/test_snowball_stemmer_sentence.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from unittest import TestCase
+
+from lib.snowball_stemmer_sentence import SnowballStemmerSentence
+
+class TestSnowballStemmerSentence(TestCase):
+    def setUp(self):
+        self.test_data = ["Szeretném"]
+        self.subject = SnowballStemmerSentence(self.test_data)
+
+    def test_sentence(self):
+        self.assertEqual(self.subject.sentence, self.test_data)
+
+    def test_result(self):
+        self.assertEqual(self.subject.result(), [u'szeretne'])
+
+    def test_result_msg(self):
+        self.assertEqual(self.subject.result_msg(), [u'szeretne'])
+
+    def test_pretty_result_msg(self):
+        self.assertEqual(self.subject.pretty_result_msg(), [u"0. 'szeretne'"])
