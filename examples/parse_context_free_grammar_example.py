71c422a7dd794f73d25d0e401a69561e1447355d
David Gerő <davidpgero@gmail.com>
2014-03-16 00:42:16 +0100

    Add CFG parser examples

--- examples/parse_context_free_grammar_example.py
+++ examples/parse_context_free_grammar_example.py
@@ -11,12 +11,16 @@ def encode(string, char_type='utf8', errors=''):
     return codecs.encode(string, char_type, errors)
 
 cfg = encode("""
-S -> NP VP NP | NP VP
+S -> NP VP NP
 NP -> 'Péter' | 'Marit' | e
 VP -> 'szereti' | e
 """)
 
 parsed_cfg = parse_cfg(cfg)
+
+parser = RecursiveDescentParser(parsed_cfg)
+uni_text = encode('Péter szereti Marit')
+sentence = word_tokenize(uni_text)
 
 parser = RecursiveDescentParser(parsed_cfg)
 uni_text = encode('Péter szereti Marit')
