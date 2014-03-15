f6bf104cbdcdb909a15c80dafe9ae2e7aebbc2f0
David Gerő <davidpgero@gmail.com>
2014-03-16 00:16:57 +0100

    Add more information to SnowballStemmer

--- examples/snowball_stemmer_example.py
+++ examples/snowball_stemmer_example.py
@@ -5,15 +5,12 @@ from __future__ import unicode_literals
 from nltk.stem.snowball import HungarianStemmer
 from nltk import word_tokenize
 
+
 stemmer = HungarianStemmer()
-test_sentence = "Szeretnék kérni tőled egy óriási szívességet az édesanyám számára."
+test_sentence = "Péter szereti Enikőt és Marit"
 
 tokenized_sentence = word_tokenize(test_sentence)
 
+print('With SnowballStemmer')
 for word in tokenized_sentence:
-    print "Original word form is '{0}' and the root is '{1}'".format(word, stemmer.stem(word))
-
-
-
-
-
+    print "Original word form is '{}' and the root is '{}'".format(word, stemmer.stem(word))
