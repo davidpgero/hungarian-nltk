9ca1416a631ccbf0d3216d952cb283448dde3f92
David Gerő <davidpgero@gmail.com>
2016-06-24 18:22:21 +0200

    example write out to output folder

--- examples/regexp_stemmer.py
+++ examples/regexp_stemmer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import csv
 from nltk.stem import RegexpStemmer
 
 
@@ -8,10 +9,19 @@ if __name__ == '__main__':
     patterns = 'i$|t$'
     regexp_stemmer = RegexpStemmer(patterns, 3)
 
-    print('With RegexpStemmer')
+    result_list = list()
     for word in ['Péter', 'szereti', 'Enikőt', 'és', 'Marit']:
         stem = regexp_stemmer.stem(word)
-        print("Original word is '{}' and the stem is '{}'".format(word, stem))
+        result_list.append([word, stem])
+
+    with open('output/regexp.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['word', 'stem'])
+        for i in result_list:
+            writer.writerow(i)
+
+    print('See the result in output/regexp.csv')
+
 
 
 
--- examples/snowball_stemmer_example.py
+++ examples/snowball_stemmer_example.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import csv
 from nltk.stem.snowball import HungarianStemmer
 from nltk import word_tokenize
 
 
-stemmer = HungarianStemmer()
-test_sentence = "Péter szereti Enikőt és Marit"
+if __name__ == '__main__':
+    stemmer = HungarianStemmer()
+    test_sentence = "Péter szereti Enikőt és Marit"
 
-tokenized_sentence = word_tokenize(test_sentence)
+    tokenized_sentence = word_tokenize(test_sentence)
 
-print('With SnowballStemmer')
-for word in tokenized_sentence:
-    print "Original word form is '{}' and the root is '{}'".format(word, stemmer.stem(word))
+    res_list = []
+    for word in tokenized_sentence:
+        stem = stemmer.stem(word)
+        res_list.append([word, stem])
+
+    with open('output/snowball.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['word', 'stem'])
+        for i in res_list:
+            writer.writerow(i)
+
+    print('See the result in output/snowball.csv')
