680cd360b9b54223e00edbee21ca73c46e23d0e9
David Gerő <davidpgero@gmail.com>
2016-06-24 18:25:51 +0200

    Fix unigramm and word_tokenizer exampler

--- examples/unigramm_tagger.py
+++ examples/unigramm_tagger.py
@@ -3,8 +3,11 @@
 from __future__ import unicode_literals, division
 from nltk.tag import UnigramTagger
 
-model = {u'Péter': 'N', 'Enikő': 'N', 'szeret': 'V'}
-tagger = UnigramTagger(model=model)
 
-print(tagger.tag(['Péter', 'Enikő', 'szeret']))
+if __name__ == '__main__':
+    model = {u'Péter': 'N', 'Enikő': 'N', 'szeret': 'V', 'Marit': 'Nacc'}
+    tagger = UnigramTagger(model=model)
+
+    print(tagger.tag(['Péter', 'Enikő', 'szeret', 'Marit']))
+
 
