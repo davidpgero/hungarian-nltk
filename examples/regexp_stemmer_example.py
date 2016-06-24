d9353ad2e16ff4df9043ea8ff1bb08dae00a51b7
David Gerő <davidpgero@gmail.com>
2016-06-24 18:02:29 +0200

    fix regexp stemmer

--- examples/regexp_stemmer_example.py
+++ examples/regexp_stemmer_example.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from nltk.stem import RegexpStemmer
 
-patterns = 'i$|t$'
-regexp_stemmer = RegexpStemmer(patterns, 3)
 
-print('With RegexpStemmer')
-for word in ['Péter', 'szereti', 'Enikőt', 'és', 'Marit']:
-    stem = regexp_stemmer.stem(word)
-    print("Original is '{}' and stem is '{}'".format(word, stem))
+if __name__ == '__main__':
+    patterns = 'i$|t$'
+    regexp_stemmer = RegexpStemmer(patterns, 3)
+
+    print('With RegexpStemmer')
+    for word in ['Péter', 'szereti', 'Enikőt', 'és', 'Marit']:
+        stem = regexp_stemmer.stem(word)
+        print("Original word is '{}' and the stem is '{}'".format(word, stem))
+
 
 
